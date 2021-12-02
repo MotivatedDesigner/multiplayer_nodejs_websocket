d33ffc839532d4bad6657145d2b3872a8b17803a
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-12-02 18:19:58 +0100

    create ws server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const http = require("http")
 const httpSrver = http.createServer()
+const websocketServer = require("websocket").server
+
 httpSrver.listen(3030, console.log('fofofo'))
+
+const wsSocket = new websocketServer({
+  "httpServer": httpSrver
+})
