23f5ad246c620312dbff1a069a5a2d84b9db68f6
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-12-02 18:49:39 +0100

    create uuid + store clientId & connection in a Map

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const httpSrver = http.createServer()
 const websocketServer = require("websocket").server
 const { v4: uuidv4 } = require('uuid')
 
-httpSrver.listen(3030, console.log('fofofo'))
+const clients = new Map()
 
-const wsSocket = new websocketServer({
-  "httpServer": httpSrver
-})
+httpSrver.listen(3030, console.log('fofofo'))
+const wsSocket = new websocketServer({"httpServer": httpSrver})
+
 wsSocket.on("request", request => {
   console.log('request',request)
 
   const connection = request.accept(null, request.origin)
+
   connection.on('open', () => console.log('open'))
   connection.on('close', () => console.log('close'))
   connection.on('message', message => {
     
   })
+
+  const clientId = uuidv4()
+  clients.set(clientId, {
+    connection
+  })
+  
 })
