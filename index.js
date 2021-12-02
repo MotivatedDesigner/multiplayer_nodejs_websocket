910b0ebce51b9bde9159f0ace660569e5beb6cbb
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-12-02 18:38:54 +0100

    adding uuid library & handling ws connection

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const http = require("http")
 const httpSrver = http.createServer()
 const websocketServer = require("websocket").server
+const { v4: uuidv4 } = require('uuid')
 
 httpSrver.listen(3030, console.log('fofofo'))
 
 const wsSocket = new websocketServer({
   "httpServer": httpSrver
+})
+wsSocket.on("request", request => {
+  console.log('request',request)
+
+  const connection = request.accept(null, request.origin)
+  connection.on('open', () => console.log('open'))
+  connection.on('close', () => console.log('close'))
+  connection.on('message', message => {
+    
+  })
 })
