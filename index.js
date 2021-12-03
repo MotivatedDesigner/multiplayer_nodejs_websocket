5a464cc5f28442c8ed971abd5d21438aa9852ba8
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-12-03 10:26:59 +0100

    create game & handling exeptions

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const websocketServer = require("websocket").server
 const { v4: uuidv4 } = require('uuid')
 
 const clients = new Map()
+const games = new Map()
 
 httpSrver.listen(3030, console.log('fofofo'))
 const wsSocket = new websocketServer({"httpServer": httpSrver})
@@ -11,28 +12,41 @@ const wsSocket = new websocketServer({"httpServer": httpSrver})
 wsSocket.on("request", request => {
 
   const connection = request.accept(null, request.origin)
-  connection.on('open', () => console.log('open'))
-  connection.on('close', () => console.log('close'))
-  connection.on('message', message => {
-    try{
-      const data = JSON.parse(message.utf8Data)
-      if(data.method === "create") console.log('data',data)
-    }
-    catch (e){
-      console.log(e)
-    }
-  })
 
   const clientId = uuidv4()
-  clients.set(clientId, {
-    connection
-  })
-
+  clients.set(clientId, { connection })
   const payload = {
     "method": "connect",
     clientId
   }
-
   connection.send(JSON.stringify(payload))
+
+  // connection.on('close', () => console.log('close'))
+
+  // connection.on('open', () => {
+    
+  // })
+  
+
+  connection.on('message', message => {
+    try{
+      const data = JSON.parse(message.utf8Data)
+      switch(data.method) {
+        case 'create':
+          const gameId = uuidv4()
+          const game = { gameId }
+          games.set(gameId, game)
+
+          const connection = clients.get(data.clientId).connection
+          if(!connection) throw new Error(`this client with id: ${data.clientId} !exist`)
+          connection.send( JSON.stringify(game) )
+        break
+
+      }
+    }
+    catch (e){
+      throw new Error('can\'t handle this kinde of data from client')
+    }
+  })
 
 })
